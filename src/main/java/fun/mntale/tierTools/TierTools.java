dc10fca9d6e1fcb1f324e1977696d0dcc0a4d1b5
MidnightTale <midnighttale24@gmail.com>
2025-06-11 14:53:23 +0700

    Initial commit

---
+++ src/main/java/fun/mntale/tierTools/TierTools.java
@@ -0,0 +1,17 @@
+package fun.mntale.tierTools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TierTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
